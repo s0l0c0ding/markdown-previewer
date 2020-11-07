cc5ca0c9e938470964706726b95778e50b794f12
s0l0c0ding <s0l0c0ding@github.com>
2020-11-07 18:35:53 +0100

    :art: [style] ant :rocket: [deploy] gh-pages

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {previwAction} from '../redux/actions';
+import { Row, Col, Card, Popover,Button } from 'antd';
+import {BulbOutlined} from '@ant-design/icons';
+import { Input } from 'antd';
+const { TextArea } = Input;
 
+const styCenter = {
+    'text-align': 'center'
+}
+const pop = (
+  <Popover content={'Just paste the markdown code to be converted in html in the right side'} 
+  trigger="click">
+       <Button shape="circle" icon= {<BulbOutlined/>}/>
+  </Popover>
+)
 class Preview extends React.Component{
     render(){
         return (
-            <div>
-                <textarea id="editor" onChange = {
-                    (event) => this.props.previwAction(event.target.value)
-                } value= {this.props.editor} type="text"
+            <Row justify='center'>
+                <Col span={12}>
+                <Card title="Markdown" bordered={true} headStyle={styCenter} extra= {pop}>
+                <TextArea id="editor"
+                onChange = {(event) => this.props.previwAction(event.target.value)}
+                value= {this.props.editor}
+                autoSize
                 />
-                    
-                <div id="preview" dangerouslySetInnerHTML= {{__html: this.props.preview}}/>
-            </div>
+                </Card>
+                </Col>
+                <Col span={12}>
+                <Card title="Html" bordered={true} headStyle={styCenter}>
+                <div  id="preview" dangerouslySetInnerHTML= {{__html: this.props.preview}}/>
+                </Card>
+                </Col>
+            </Row>
         )
     }
 }
